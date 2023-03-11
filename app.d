4dec5bf5a6c2ab2d6b1e4390829c95f03368f585
Dappstr <87837549+Dappstr@users.noreply.github.com>
2023-03-11 12:07:08 -0800

    Create app.d

---
+++ app.d
@@ -0,0 +1,8 @@
+module source.app;
+
+import std.stdio;
+
+void main() {
+    //readf("%s".chomp(), &someVar) <- .chomp() removes newline
+	  //Safe calculations: adds/addu, subs/subu, muls/mulu <- s = signed, u = unsigned
+}
